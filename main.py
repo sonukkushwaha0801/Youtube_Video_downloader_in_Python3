ab022411ed30b0f49a0c29e0e59f660d1bb535ba
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2023-10-24 09:03:27 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-# For installing the pytube
-# pip install -r Requirement
-
 import pytube
-Downloading_URL = input("Enter the URL of the video you want to download: ")
-youtube = pytube.YouTube(Downloading_URL)
-steam = youtube.streams.get_highest_resolution()
-print("Start Downloading")
-steam.download()
-print("Video downloaded")
+
+# Get the video URL from the user
+🚀 Downloading_URL = input("🌟 Enter the URL of the video you want to download: ")
+
+# Create a YouTube object
+📺 youtube = pytube.YouTube(🚀 Downloading_URL)
+
+# Get the highest resolution stream
+🌈 stream = youtube.streams.get_highest_resolution()
+
+# Let's get this party started!
+print("🚀 Start Downloading")
+
+# Download the video
+🌊 stream.download()
+
+# Hooray, your video is now on your device! 🎉📽️
+print("🎬 Video downloaded")
