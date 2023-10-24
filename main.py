7544ac6a5e3d161da5c40a750c577506e2e56816
Sonu Kumar Kushwaha <144480219+sonukkushwaha0801@users.noreply.github.com>
2023-10-24 08:53:19 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,7 @@
+import pytube
+Downloading_URL = input("Enter the URL of the video you want to download: ")
+youtube = pytube.YouTube(Downloading_URL)
+steam = youtube.streams.get_highest_resolution()
+print("Start Downloading")
+steam.download()
+print("Video downloaded")
